d9e07ca46cb9857c560a38a4fdd559284da6f039
Lewis_Damy <ldamy16@gmail.com>
2024-06-25 15:20:15 -0300

    ✨ New template BadgeExample for Deeper dive into components

--- vue-study-app/src/main.js
+++ vue-study-app/src/main.js
@@ -1,22 +1,16 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
-// import FriendContact from "./components/FriendContact.vue";
-// import NewFriend from "./components/NewFriend.vue";
-import ActiveElement from './components/Knowledge/ActiveElement.vue';
-import KnowledgeBase from './components/Knowledge/KnowledgeBase.vue';
-import KnowledgeElement from './components/Knowledge/KnowledgeElement.vue';
-import KnowledgeGrid from './components/Knowledge/KnowledgeGrid.vue';
+import TheHeader from "./components/BadgeExample/TheHeader.vue";
+import BaseBadge from "./components/BadgeExample/BaseBadge.vue";
+import BadgeList from "./components/BadgeExample/BadgeList.vue";
+import UserInfo from "./components/BadgeExample/UserInfo.vue";
 
 const app = createApp(App);
 
-// app.component("friend-contact", FriendContact);
-// app.component("new-friend", NewFriend);
-
-
-app.component('active-element', ActiveElement);
-app.component('knowledge-base', KnowledgeBase);
-app.component('knowledge-element', KnowledgeElement);
-app.component('knowledge-grid', KnowledgeGrid);
+app.component("the-header", TheHeader);
+app.component("base-badge", BaseBadge);
+app.component("badge-list", BadgeList);
+app.component("user-info", UserInfo);
 
 app.mount("#app");
