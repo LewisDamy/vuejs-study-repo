4c0788c71bb0c3db912bf080eabad4aea891a667
Lewis_Damy <ldamy16@gmail.com>
2024-06-24 23:16:44 -0300

    ✨ Provide-Inject basic logic

--- vue-study-app/src/main.js
+++ vue-study-app/src/main.js
@@ -1,12 +1,22 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import FriendContact from "./components/FriendContact.vue";
-import NewFriend from "./components/NewFriend.vue";
+// import FriendContact from "./components/FriendContact.vue";
+// import NewFriend from "./components/NewFriend.vue";
+import ActiveElement from './components/Knowledge/ActiveElement.vue';
+import KnowledgeBase from './components/Knowledge/KnowledgeBase.vue';
+import KnowledgeElement from './components/Knowledge/KnowledgeElement.vue';
+import KnowledgeGrid from './components/Knowledge/KnowledgeGrid.vue';
 
 const app = createApp(App);
 
-app.component("friend-contact", FriendContact);
-app.component("new-friend", NewFriend);
+// app.component("friend-contact", FriendContact);
+// app.component("new-friend", NewFriend);
+
+
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
 
 app.mount("#app");
