226bd608f0f5ef12569d72045df73e417980836b
Lewis_Damy <ldamy16@gmail.com>
2024-06-26 12:53:51 -0300

    🚚 Move App.vue and main.js from BadgeExample to it correspondent folder

---
+++ vue-study-app/src/components/BadgeExample/App.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<the-header></the-header>
+		<badge-list></badge-list>
+		<user-info
+			:full-name="activeUser.name"
+			:info-text="activeUser.description"
+			:role="activeUser.role"
+		></user-info>
+		<course-goals #default="slotProps">
+			<!-- <template #default="slotProps"> used only when there's only one slot available -->
+			<h2>{{ slotProps.item }}</h2>
+			<p>{{ slotProps["another-prop"] }}</p>
+			<!-- </template> -->
+		</course-goals>
+	</div>
+</template>
+
+<script>
+import TheHeader from "./components/BadgeExample/TheHeader.vue";
+import BadgeList from "./components/BadgeExample/BadgeList.vue";
+import UserInfo from "./components/BadgeExample/UserInfo.vue";
+import CourseGoals from "./components/BadgeExample/CourseGoals.vue";
+
+export default {
+	components: {
+		"the-header": TheHeader,
+		// "badge-list": BadgeList === BadgeList,
+		BadgeList,
+		UserInfo,
+		CourseGoals,
+	},
+	data() {
+		return {
+			activeUser: {
+				name: "Lewis D",
+				description: "Site owner and admin",
+				role: "admin",
+			},
+		};
+	},
+};
+</script>
+
+<style>
+html {
+	font-family: sans-serif;
+}
+
+body {
+	margin: 0;
+}
+</style>
