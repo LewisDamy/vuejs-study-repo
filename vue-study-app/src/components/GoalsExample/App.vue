386a002ae939bbda87313ee23c8fbbdff45034dc
Lewis_Damy <ldamy16@gmail.com>
2024-06-26 18:39:21 -0300

    ✨ Form logic done
    Explained v-model(modifiers and numbers, dropdowns, checkbox&radiobuttons), basic form validation. Creating a custom Control Component and using a v-model in it.

---
+++ vue-study-app/src/components/GoalsExample/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<button @click="setSelectedComponent('active-goals')">Active Goals</button>
+		<button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+		<!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+		<manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+		<!-- allows to conditionally cache component instances when dynamically switching between multiple components -->
+		<keep-alive>
+			<component :is="selectedComponent"></component>
+		</keep-alive>
+	</div>
+</template>
+
+<script>
+import ActiveGoals from "./ActiveGoals.vue";
+import ManageGoals from "./ManageGoals.vue";
+
+export default {
+	components: {
+		ManageGoals,
+		ActiveGoals,
+	},
+	data() {
+		return {
+			selectedComponent: "active-goals",
+		};
+	},
+	methods: {
+		setSelectedComponent(cmp) {
+			this.selectedComponent = cmp;
+		},
+	},
+};
+</script>
+
+<style>
+html {
+	font-family: sans-serif;
+}
+
+body {
+	margin: 0;
+}
+</style>
