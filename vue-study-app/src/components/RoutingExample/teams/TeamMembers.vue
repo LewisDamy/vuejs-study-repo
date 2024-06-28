e0c825a0266452ed63c5e31047099743db03288c
Lewis_Damy <ldamy16@gmail.com>
2024-06-28 13:45:17 -0300

    ✨ Added query params + accessing in on the page

--- vue-study-app/src/components/RoutingExample/teams/TeamMembers.vue
+++ vue-study-app/src/components/RoutingExample/teams/TeamMembers.vue
@@ -44,6 +44,8 @@ export default {
 	created() {
 		// this.$route.path // /teams/t1
 		this.loadTeamMembers(this.teamId);
+        // acessing query params
+		console.log(this.$route.query);
 	},
 	watch: {
 		teamId(newId) {
