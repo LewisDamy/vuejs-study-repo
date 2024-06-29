b5298146c47aa459e774062e7dbf1d7c9c964356
Lewis_Damy <ldamy16@gmail.com>
2024-06-29 19:51:14 -0300

    ✨ Basic of vuex usage
    Utilization of state and mutation to add 2 in button across multiple componentsj

---
+++ vue-study-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+	<base-container title="Vuex">
+		<h3>{{ counter }}</h3>
+		<the-counter></the-counter>
+		<button @click="addOne">Add 1</button>
+		<change-counter></change-counter>
+	</base-container>
+</template>
+
+<script>
+import BaseContainer from "./vuex/BaseContainer.vue";
+import ChangeCounter from "./vuex/ChangeCounter.vue";
+import TheCounter from "./vuex/TheCounter.vue";
+
+export default {
+	components: {
+		BaseContainer,
+		TheCounter,
+		ChangeCounter,
+	},
+	methods: {
+		addOne() {
+			this.$store.commit("increment");
+		},
+	},
+};
+</script>
+
+<style>
+* {
+	box-sizing: border-box;
+}
+
+html {
+	font-family: sans-serif;
+}
+
+body {
+	margin: 0;
+}
+</style>
