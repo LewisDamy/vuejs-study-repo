5f96b6c0589fff4dbeefda38731035c2e9a80b2a
Lewis_Damy <ldamy16@gmail.com>
2024-07-03 09:02:21 -0300

    ✨ Working with watches

--- vue-study-app/src/App.vue
+++ vue-study-app/src/App.vue
@@ -1,26 +1,48 @@
 <template>
 	<section class="container">
-		<h2>{{ user.name }}</h2>
-		<h2>{{ user.age }}</h2>
+		<h2>{{ userName }}</h2>
+		<h2>{{ age }}</h2>
 		<button @click="setAge">Change Age</button>
+		<div>
+			<input type="text" placeholder="First Name" @input="firstName" />
+			<input type="text" placeholder="Last Name" @input="lastName" />
+		</div>
 	</section>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { computed, ref, watch } from "vue";
 
 export default {
 	setup() {
-		const user = reactive({
-			name: "Me",
-			age: 23,
+		// const user = reactive({
+		// 	name: "Me",
+		// 	age: 23,
+		// });
+		const firstName = ref("");
+		const lastName = ref("");
+		const uAge = ref(23);
+
+		const uName = computed(() => {
+			return firstName.value + " " + lastName.value;
+		});
+
+		watch([uAge, uName], (newValue, oldValue) => {
+			console.log("Old age: " + oldValue);
+			console.log("New age: " + newValue);
 		});
 
 		const setNewAge = () => {
-			user.age = 24;
+			uAge.value = 24;
 		};
 
-		return { user: user, setAge: setNewAge };
+		return {
+			userName: uName,
+			age: uAge,
+			setAge: setNewAge,
+			firstName,
+			lastName,
+		};
 	},
 };
 </script>
